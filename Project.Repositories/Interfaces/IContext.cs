2b2e72470ccd86d74329b83affdeb3bf18f3eeee
unknown <MiCohen@madisoncres.com>
2023-02-08 20:21:25 +0200

    first

---
+++ Project.Repositories/Interfaces/IContext.cs
@@ -0,0 +1,12 @@
+﻿using Project.Repositories.Entities;
+
+namespace Project.Repositories.Interfaces
+{
+    public interface IContext
+    {
+        DbSet<Person> Persons { get; set; }
+        DbSet<Child> Children { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Project.Services/Mapping.cs
@@ -0,0 +1,14 @@
+﻿using Project.Common.DTOs;
+using Project.Repositories.Entities;
+
+namespace Project.Services
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Person, PersonDTO>().ReversMap();
+            CreateMap<Child, ChildDTO>().ReversMap();
+        }
+    }
+}
